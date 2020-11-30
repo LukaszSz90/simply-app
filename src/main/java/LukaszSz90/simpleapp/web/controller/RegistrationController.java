97d3637bf532716a070d942423f1ceb76346d864
Lukasz Szewczuk <lukasz90sz@gmail.com>
2020-11-30 14:45:18 +0100

    structure :: add controller for registration

---
+++ src/main/java/LukaszSz90/simpleapp/web/controller/RegistrationController.java
@@ -0,0 +1,34 @@
+package LukaszSz90.simpleapp.web.controller;
+
+import LukaszSz90.simpleapp.web.command.RegisterUserCommand;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.constraints.Size;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @GetMapping
+    public String getRegister() {
+        return "register/form";
+    }
+
+    @PostMapping
+    public String processRegister(RegisterUserCommand registerUserCommand, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "register/form";
+        }
+
+        return "redirect:/login";
+    }
+}
