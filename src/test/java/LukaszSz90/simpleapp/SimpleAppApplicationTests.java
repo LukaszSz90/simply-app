de325d09698a7692d181cc432ea35625cd75b0f5
Lukasz Szewczuk <lukasz90sz@gmail.com>
2020-11-30 12:01:31 +0100

    structure :: initial project structure

---
+++ src/test/java/LukaszSz90/simpleapp/SimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package LukaszSz90.simpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
