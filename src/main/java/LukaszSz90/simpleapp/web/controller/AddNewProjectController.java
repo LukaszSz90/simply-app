0792bc408377b5d32de7ae5c91cb2e3f8c81523f
Lukasz Szewczuk <lukasz90sz@gmail.com>
2020-12-02 13:58:29 +0100

    project class :: add controller

---
+++ src/main/java/LukaszSz90/simpleapp/web/controller/AddNewProjectController.java
@@ -0,0 +1,23 @@
+package LukaszSz90.simpleapp.web.controller;
+
+import LukaszSz90.simpleapp.web.command.CreateProjectCommand;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+@Slf4j
+@RequiredArgsConstructor
+public class AddNewProjectController {
+
+    @GetMapping
+    public String getAddProjectPage(Model model) {
+        model.addAttribute(new CreateProjectCommand());
+        return "project/add";
+    }
+
+}
